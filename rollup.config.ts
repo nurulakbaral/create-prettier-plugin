68184e77f2c2129c57c80d872259f0b88842e1f6
Nurul Akbar Al-Ghifari <nurulakbaral@gmail.com>
2024-04-21 11:57:55 +0700

    Chore: Change rollup config to typescript

---
+++ rollup.config.ts
@@ -0,0 +1,16 @@
+import type { RollupOptions } from 'rollup'
+import commonjs from '@rollup/plugin-commonjs'
+import typescript from '@rollup/plugin-typescript'
+
+const config: RollupOptions = {
+  input: 'src/index.ts',
+  output: {
+    dir: 'dist',
+    format: 'esm',
+    sourcemap: true,
+  },
+  external: ['prettier', 'prettier/plugins/babel', 'prettier/plugins/typescript', 'prettier/plugins/estree'],
+  plugins: [commonjs(), typescript()],
+}
+
+export default config
